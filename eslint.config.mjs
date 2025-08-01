4d72743a7507ad82900abb4e2754c7f7011ab8a9
Muhammad Rasyid Ridho <emrasy.ridho@gmail.com>
2025-08-01 23:53:11 +0700

    Fix commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // 🔧 Matikan error any
+      "@typescript-eslint/no-unused-vars": "warn", // (opsional)
+      "react/no-unescaped-entities": "off",        // (opsional)
+    },
+  },
 ];
 
 export default eslintConfig;
